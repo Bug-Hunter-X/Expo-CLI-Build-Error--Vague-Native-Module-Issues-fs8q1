83609c0b3208bd5b906677a2608d10ddb0ef1c37
BugHunter-X <askzenai@gmail.com>
2024-12-09 01:42:09 +0100

    Add expoBug.js file

---
+++ expoBug.js
@@ -0,0 +1,1 @@
+This error occurs when using the Expo CLI with a project that has a dependency on a native module that isn't properly configured for the Expo managed workflow.  This often manifests as a build error during the `expo prebuild` or `expo build` process.  The error message might not be very specific, potentially mentioning issues with linking or native code compilation.
